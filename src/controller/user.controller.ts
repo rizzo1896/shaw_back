6ec33ca5c1d340edd64c0c08c70f3510ea2f3a02
Gabriel Rizzo <daudtrizzo@gmail.com>
2022-12-28 15:01:06 -0300

    feat: create user controller and setting server

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,63 @@
+import { Router } from "express";
+import githubServices from "../service/github.services";
+
+class UserController {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.routes();
+  }
+
+  public routes() {
+    this.router.get("/", async (req, res) => {
+      const { since, pagination } = req.query;
+      try {
+        const response = await githubServices.getUserList({
+          since: Number(since),
+          pagination: Number(pagination || 10),
+        });
+
+        if (response.length === 0) {
+          return res.status(404).json({ message: "No users found" });
+        }
+
+        return res.status(200).json(response);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    });
+
+    this.router.get("/:username/details", async (req, res) => {
+      const { username } = req.params;
+      try {
+        const response = await githubServices.getUserDetails(username);
+
+        if (!response) {
+          return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json(response);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    });
+
+    this.router.get("/:username/repos", async (req, res) => {
+      const { username } = req.params;
+      try {
+        const response = await githubServices.getUserRepos(username);
+
+        if (!response) {
+          return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json(response);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    });
+  }
+}
+
+export const userRoutes = new UserController();
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { config } from "dotenv";
+import { userRoutes } from "./controller/user.controller";
 config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes.router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
