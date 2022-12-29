b63afc5435581f22e4a21a61c188eb0552af1f1a
Gabriel Rizzo <daudtrizzo@gmail.com>
2022-12-29 13:15:03 -0300

    feat: added cors policy

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { config } from "dotenv";
 import { userRoutes } from "./controller/user.controller";
+import Cors from "cors";
 config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(Cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/users", userRoutes.router);
